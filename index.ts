ffb0b9cc45329b44aaddb575c077634b812c6953
SaiMaiNow <commanchannel@gmail.com>
2024-12-14 02:07:25 +0700

    Set up EJS as the view engine and add initial views and layout

--- index.ts
+++ index.ts
@@ -4,10 +4,11 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req: any, res: any) => {
-  res.send('Hello World!');
-});
+app.set('view engine', 'ejs');
 
+app.get('/', (req: any, res: any) => {
+  res.render('pages/index.ejs');
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
