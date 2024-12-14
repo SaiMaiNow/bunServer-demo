245e3fa54ba9c6fc1830a74438d9d646f119614e
SaiMaiNow <commanchannel@gmail.com>
2024-12-15 00:09:57 +0700

    Add login and signup pages, and replace navbar component

--- index.ts
+++ index.ts
@@ -10,6 +10,14 @@ app.get('/', (req: any, res: any) => {
   res.render('pages/index.ejs');
 });
 
+app.get('/login', (req: any, res: any) => {
+  res.render('pages/login.ejs');
+});
+
+app.get('/signup', (req: any, res: any) => {
+  res.render('pages/signup.ejs');
+});
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
